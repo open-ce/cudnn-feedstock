6c12d4e837682a106bfa5cf8658828ecb2e0b8e3
Deepali Chourasia <deepch23@us.ibm.com>
2021-05-05 09:43:19 -0400

    use cudnn archives

---
+++ cudnn8.1/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# *****************************************************************
+# (C) Copyright IBM Corp. 2018, 2021. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# *****************************************************************
+
+CUDNN_VERSION=${PKG_VERSION%%_*}
+mkdir $PREFIX/include
+mkdir -p $PREFIX/doc/nvidia/
+mkdir -p $PREFIX/lib64
+mkdir -p $PREFIX/cuda/lib
+
+cp $RECIPE_DIR/LICENSE $PREFIX/doc/nvidia/cuDNN_LICENSE
+cp -r $SRC_DIR/cudnn/lib64/ $PREFIX/lib/
+
+
+# The archive has version subscripts meant to be used for
+# the `alternatives` facility in Linux. No need for that here
+# so we strip them out of the file names.
+
+rename _v8 '' $SRC_DIR/cudnn/include/*
+cp -r $SRC_DIR/cudnn/include $PREFIX
